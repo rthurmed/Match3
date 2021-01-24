bfc6c66d6bea7b9d0986111986c416dc3be722ad
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-01-24 15:21:18 -0300

    Draggable cells

--- src/Cell.gd
+++ src/Cell.gd
@@ -2,15 +2,27 @@ extends Control
 
 
 onready var sprite: Sprite = $Center/Sprite
+onready var tween: Tween = $Tween
+
 export var value = 0
 export var id = 0
 var selected = false
 var initial_sprite_position = Vector2(0, 0)
+var valid_directions = [
+	Vector2.DOWN,
+	Vector2.UP,
+	Vector2.LEFT,
+	Vector2.RIGHT
+]
+
+signal dropped(id, dir)
 
 
 func _input(event):
 	if selected and event is InputEventMouseMotion:
 		sprite.position += event.relative
+		sprite.position.x = clamp(sprite.position.x, -42, 42)
+		sprite.position.y = clamp(sprite.position.y, -42, 42)
 
 
 func _ready():
@@ -19,7 +31,7 @@ func _ready():
 
 func _process(_delta):
 	sprite.z_index = 100 if selected else 1
-	sprite.scale = Vector2.ONE * (1.5 if selected else 1.0)
+	sprite.scale = Vector2.ONE * (1.25 if selected else 1.0)
 
 func set_value(v):
 	value = v
@@ -30,10 +42,21 @@ func update_sprite():
 	sprite.region_rect.position.x = sprite.region_rect.size.x * value
 
 
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_FOCUS_OUT or what == MainLoop.NOTIFICATION_WM_MOUSE_EXIT:
+		selected = false
+		sprite.position = Vector2.ZERO
+
+
 func _on_Cell_gui_input(event: InputEvent):
 	if event.is_action_pressed("click"):
 		selected = true
 	
 	if event.is_action_released("click"):
 		selected = false
+		
+		var dir = sprite.position.normalized().round()
+		if valid_directions.has(dir):
+			emit_signal("dropped", id, dir)
+		
 		sprite.position = Vector2.ZERO
--- src/Game.gd
+++ src/Game.gd
@@ -12,6 +12,8 @@ const CELL_EMPTY_VALUE = -1
 
 onready var Cell = preload("res://src/Cell.tscn")
 
+onready var grid: GridContainer = $Grid
+
 var rng = RandomNumberGenerator.new()
 var cells = []
 
@@ -25,15 +27,42 @@ func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
 	if event.is_action_pressed("reset"):
+		# warning-ignore:return_value_discarded
 		get_tree().reload_current_scene()
 
 
 func build_grid():
 	for i in range(0, CELL_TOTAL):
 		var cell = Cell.instance()
+		cell.connect("dropped", self, "_on_Cell_dropped")
 		
 		cell.value = rng.randi_range(CELL_MIN_VALUE, CELL_MAX_VALUE)
 		cell.id = i
 		
-		$Grid.add_child(cell, true)
+		grid.add_child(cell, true)
 		cells.insert(i, cell)
+
+
+func get_cell_position_by_id(id):
+	var x = id % CELL_COLS
+	var y = id / CELL_ROWS
+	return Vector2(x, y)
+
+
+func get_cell_id_by_position(pos: Vector2):
+	return pos.y * CELL_ROWS + pos.x
+
+
+func _on_Cell_dropped(id, dir):
+	var caller_pos = get_cell_position_by_id(id)
+	var target_pos = caller_pos + dir
+	var target_id = get_cell_id_by_position(target_pos)
+	
+	# switch values
+	var old = cells[target_id].value
+	cells[target_id].set_value(cells[id].value)
+	cells[id].set_value(old)
+	
+	print(caller_pos, " + ", dir, " = ", target_pos, "; ", id, " => ", target_id)
+	
+	# TODO: Clean adjacent tiles
